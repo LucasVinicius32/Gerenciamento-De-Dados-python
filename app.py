016c95682980910ff98213e85c9c06dd330881bf
LucasVinicius32 <lucasviniciuswinner2012@gmail.com>
2022-06-18 17:20:56 -0300

    Manipulando Dados Excel

---
+++ app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from twilio.rest import Client
+
+lista_meses = ['janeiro','fevereiro','março','abril','maio','junho']
+
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000,'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000,'Vendas'].values[0]
+        print(f'No mês {mes} alguem bateu a meta. Vendedor: {vendedor}, vendas: {vendas}')
+        
+# Your Account SID from twilio.com/console
+account_sid = "#"
+# Your Auth Token from twilio.com/console
+auth_token  = "#your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="#+15558675309", 
+    from_="#+15017250604",
+    body="#Hello from Python!")
+
+print(message.sid)
